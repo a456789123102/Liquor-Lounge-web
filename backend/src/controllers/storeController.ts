33b6c24e4b46f38d2d40b6d66018563280f9be79
456789123102 <aize.gong@gmail.com>
2024-09-30 13:02:28 +0700

    up to date createstore

--- backend/src/controllers/storeController.ts
+++ backend/src/controllers/storeController.ts
@@ -54,7 +54,58 @@ export const branches = async (req: Request, res: Response) => {
       },
     });
 
-    res.status(200).json(branches);
+    res.status(200).json(branches); // ส่งข้อมูล branches กลับมาในรูปแบบ JSON
+  } catch (error) {
+    res.status(500).json({ error: String(error) });
+  }
+};
+
+
+//หา store เดียว
+export const findOne = async (req: Request, res: Response) => {
+  try {
+    const { storeId } = req.params;
+    const store = await prisma.store.findUnique({
+      where: {
+        id: parseInt(storeId),
+        ownerId: (req as CustomRequest).user.id,
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    if (!store) {
+      return res.status(404).json({ message: "Store not found" });
+    }
+    return res.status(200).json(store);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//update store
+export const update = async (req: Request, res: Response) => {
+  try {
+    const { storeId } = req.params;
+    const { name } = req.body;
+
+    const store = await prisma.store.update({
+      where: {
+        id: parseInt(storeId),
+      },
+      data: {
+        name: name,
+      },
+    });
+
+    if (!store) {
+      return res.status(404).json({ error: "Store not found" });
+    }
+
+    res.status(200).json(store);
   } catch (error) {
     res.status(500).json({ error: String(error) });
   }
--- backend/src/routes/storeRoute.ts
+++ backend/src/routes/storeRoute.ts
@@ -1,4 +1,4 @@
-import {create, find,branches} from '../controllers/storeController';
+import {create, find,branches, findOne, update} from '../controllers/storeController';
 import { Router } from "express";
 
 const router = Router();
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/",create);
 router.get("/",find);
 router.get("/:storeId/branches",branches);
-
+router.get("/:storeId",findOne);
+router.patch("/:storeId",update);
 export default router;
